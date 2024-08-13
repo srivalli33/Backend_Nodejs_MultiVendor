44c39d9b50926defcb2d53fc933e4ebe561c762e
srivalli <srivalli@lyrostech.com>
2024-08-13 12:58:22 +0530

    First time pushing my Backend project to git

---
+++ controllers/vendorController.js
@@ -0,0 +1,81 @@
+const Vendor = require('../models/Vendor');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const dotEnv = require('dotenv');
+
+dotEnv.config();
+
+const secretKey = process.env.WhatIsYourName;
+
+const vendorRegister = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+    const existingVendor = await Vendor.findOne({ email });
+    if (existingVendor) {
+      return res.status(400).json({ error: "Email already taken" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newVendor = new Vendor({
+      username,
+      email,
+      password: hashedPassword
+    });
+
+    await newVendor.save();
+
+    res.status(201).json({ message: "Vendor registered successfully" });
+    console.log('Vendor registered successfully');
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+const vendorLogin = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const vendor = await Vendor.findOne({ email });
+    if (!vendor || !(await bcrypt.compare(password, vendor.password))) {
+      return res.status(401).json({ error: "Invalid email or password" });
+    }
+
+    const token = jwt.sign({ vendorId: vendor._id }, secretKey, { expiresIn: "1h" });
+
+    res.status(200).json({ success: "Login successful", token });
+    console.log('Vendor logged in:', email, "this is token:", token);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+const getAllVendors = async (req, res) => {
+  try {
+    const vendors = await Vendor.find().populate('firm');
+    res.json({ vendors });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+const getVendorById = async (req, res) => {
+  const vendorId = req.params.id;  // Assuming "id" is the correct parameter name
+
+  try {
+    const vendor = await Vendor.findById(vendorId);
+    if (!vendor) {
+      return res.status(404).json({ error: "Vendor not found" });
+    }
+    res.status(200).json(vendor);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+module.exports = { vendorRegister, vendorLogin, getAllVendors, getVendorById };
---
+++ models/Vendor.js
@@ -0,0 +1,28 @@
+
+const mongoose = require('mongoose');
+
+const VendorSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    firm: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Firm'
+        }
+    ]
+});
+
+const Vendor = mongoose.model('Vendor',VendorSchema);
+
+module.exports = Vendor;
